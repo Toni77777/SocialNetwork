62badef505557250820699a2c3327dc826b3a6c9
toxa7777 <toxa95401@gmail.com>
2016-07-11 19:57:20 +0300

    Update action lambda at LoginPresenter

--- app/src/main/java/by/grodno/toni7777/socialnetwork/login/LoginPresenterImp.java
+++ app/src/main/java/by/grodno/toni7777/socialnetwork/login/LoginPresenterImp.java
@@ -1,11 +1,11 @@
 package by.grodno.toni7777.socialnetwork.login;
 
+import android.util.Log;
+
 import by.grodno.toni7777.socialnetwork.network.NetworkService;
 import by.grodno.toni7777.socialnetwork.test.UserLogin;
 import rx.Observable;
-import rx.Observer;
 import rx.Subscription;
-import rx.functions.Action1;
 
 public class LoginPresenterImp implements LoginPresenter {
 
@@ -23,14 +23,10 @@ public class LoginPresenterImp implements LoginPresenter {
         Observable<UserLogin> loginObservable = (Observable<UserLogin>)
                 networkService.getPreparedObservable(networkService.getLoginService().loginRequest(login, password));
 
-        subscription = loginObservable.subscribe(userLogin -> {
-            loginView.loginSuccess(userLogin);
-
-        }, throwable -> {
-            loginView.loginError(throwable);
-        });
+        subscription = loginObservable.subscribe(
+                userLogin -> loginView.loginSuccess(userLogin),
+                throwable -> loginView.loginError(throwable));
     }
-
 
     @Override
     public void rxUnSubscribe() {
