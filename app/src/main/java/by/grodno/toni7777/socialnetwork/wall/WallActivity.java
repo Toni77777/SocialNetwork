dc2723ecc92acb0f1909f6195b3fd35429dc71e9
toxa7777 <toxa95401@gmail.com>
2016-08-02 17:24:03 +0300

    Wall add scroll toolbar

--- app/src/main/java/by/grodno/toni7777/socialnetwork/wall/WallActivity.java
+++ app/src/main/java/by/grodno/toni7777/socialnetwork/wall/WallActivity.java
@@ -3,14 +3,14 @@ package by.grodno.toni7777.socialnetwork.wall;
 import android.os.Bundle;
 
 import by.grodno.toni7777.socialnetwork.R;
-import by.grodno.toni7777.socialnetwork.base.DrawerActivity;
+import by.grodno.toni7777.socialnetwork.base.ToolbarActivity;
 
-public class WallActivity extends DrawerActivity {
+public class WallActivity extends ToolbarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawer);
+        setContentView(R.layout.activity_toolbar);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
