61ccc413d81e81e9a62e38a2c211462f79246ce6
toxa7777 <toxa95401@gmail.com>
2016-08-29 00:24:33 +0300

    Add MessagesActivity

---
+++ app/src/main/java/by/grodno/toni7777/socialnetwork/ui/messages/MessagesFragment.java
@@ -0,0 +1,6 @@
+package by.grodno.toni7777.socialnetwork.ui.messages;
+
+import android.support.v4.app.Fragment;
+
+public class MessagesFragment extends Fragment {
+}
---
+++ app/src/main/java/by/grodno/toni7777/socialnetwork/ui/messages/adapter/MessagesAdapter.java
@@ -0,0 +1,4 @@
+package by.grodno.toni7777.socialnetwork.ui.messages.adapter;
+
+public class MessagesAdapter {
+}
